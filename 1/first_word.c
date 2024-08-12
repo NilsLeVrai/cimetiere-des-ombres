bad107e944cea2ae8631f2f9f05420386599e629
NilsLeVrai <nilslucas@wanadoo.fr>
2024-08-12 18:07:24 +0200

    sombre

--- 1/first_word.c
+++ 1/first_word.c
@@ -5,7 +5,16 @@ int main(int argc, char **argv)
 	int i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i])
+		while (argv[1][i] != '\0')
+		{
+			while (argv[1][i] == 32 || argv[1][i] == 9)
+				i++;
+			while (!(argv[1][i] == 32 || argv[1][i] == 9))
+			{
+				write(1, &argv[1][i++], 1);
+				//break ;
+			}
+		}
+		write(1, "\n", 1);
 	}
-	write(1, "\n", 1);
 }
